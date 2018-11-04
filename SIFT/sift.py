1554645cf7734718faa9d2904570e91ef3cb667c
Ameya Daigavane <ameya.d.98@gmail.com>
2018-11-04 15:12:15 +0530

    Derivative masks implemented. New base CV class.

--- SIFT/sift.py
+++ SIFT/sift.py
@@ -8,16 +8,16 @@ from PIL import Image
 import numpy as np
 
 
-class SIFT:
+# A generic CV class for specific algorithms to inherit from
+class CV:
     def __init__(self):
         self.image_original = None
         self.image_grayscale = None
         self.image_original_array = None
         self.image_grayscale_array = None
-        self.image_marked = None
 
     @staticmethod
-    # creates a gaussian blur matrix of shape (size, size) where size is the closest odd integer to 6*scale.
+    # creates a Gaussian filter matrix of shape (size, size) where size is the closest odd integer greater than 6*scale.
     def gaussian_blur_matrix(scale=1):
         size = 2*int(3*scale) + 1
         temp1 = np.square(np.abs(np.ceil(np.mgrid[-size / 2: size / 2, -size / 2: size / 2][0])))
@@ -25,6 +25,15 @@ class SIFT:
         blur_matrix = np.exp(-(temp1 + temp2)/(2*(scale**2)))
         blur_matrix /= np.sum(blur_matrix)
         return blur_matrix
+
+    @staticmethod
+    # creates a pair of Sobel operator matrices of shape (3, 3) each.
+    def sobel_matrix():
+        operator_matrix = -np.mgrid[-1:2, -1:2]
+        operator_matrix[1][1] *= 2
+        operator_matrix[0] = operator_matrix[1]
+        operator_matrix[1] = np.transpose(operator_matrix[1])
+        return operator_matrix
 
     @staticmethod
     # 2D convolution of m2 with m1
@@ -57,6 +66,16 @@ class SIFT:
         grayscale_img = Image.fromarray(a)
         grayscale_img.show()
 
+    @staticmethod
+    # applies the gaussian blur of specified scale to the 2D image array
+    def apply_blur(imgarray, scale=1):
+        return CV.convolve(CV.gaussian_blur_matrix(scale), imgarray)
+
+    @staticmethod
+    # applies a Sobel derivative mask to the 2D image array
+    def apply_derivative(imgarray):
+        return np.array([CV.convolve(CV.sobel_matrix()[0], imgarray), CV.convolve(CV.sobel_matrix()[1], imgarray)])
+
     # open a image file, associate PIL objects, and get array representations
     def open_file(self, image_path):
         self.image_original = Image.open(image_path)
@@ -65,18 +84,6 @@ class SIFT:
         self.image_grayscale.load()
         self.image_original_array = np.asarray(self.image_original, dtype="uint32")
         self.image_grayscale_array = np.asarray(self.image_grayscale, dtype="uint32")
-
-    # obtain SIFT descriptors - the actual algorithm
-    def detect(self):
-        # Scale-space Extrema Detection - Difference of Gaussians
-        m1 = self.convolve(SIFT.gaussian_blur_matrix(3.2), self.image_grayscale_array)
-        m2 = self.convolve(SIFT.gaussian_blur_matrix(2), self.image_grayscale_array)
-
-        return np.abs(np.abs(m1 - m2) - 255)
-
-    # applies the gaussian blur of specified scale to the grayscale image
-    def apply_blur(self, scale=1):
-        return self.convolve(SIFT.gaussian_blur_matrix(scale), self.image_grayscale_array)
 
     # get the PIL Image object tied to the original image
     def get_original(self):
@@ -90,18 +97,37 @@ class SIFT:
     def show_grayscale(self):
         self.image_grayscale.show()
 
+
+# The SIFT Detector Class
+class SIFT(CV):
+    def __init__(self):
+        super().__init__()
+        self.image_marked = None
+
     # get boxed features on top of image
     def get_marked(self):
         return self.image_marked
+
+    # obtain SIFT descriptors - the actual SIFT algorithm
+    def detect(self):
+        # Scale-space Extrema Detection - Difference of Gaussians
+        m1 = self.convolve(SIFT.gaussian_blur_matrix(2), self.image_grayscale_array)
+        m2 = self.convolve(SIFT.gaussian_blur_matrix(1), self.image_grayscale_array)
+
+        return np.abs(np.abs(m1 - m2) - 255)
 
 
 if __name__ == "__main__":
     np.set_printoptions(precision=1)
 
     SIFT_sample = SIFT()
+
     SIFT_sample.open_file('mike.jpg')
 
     print(SIFT_sample.image_grayscale_array)
 
-    SIFT_sample.show_grayscale()
-    SIFT_sample.display_array(SIFT_sample.detect())
+    deriv = SIFT_sample.apply_derivative(SIFT.apply_blur(SIFT_sample.image_grayscale_array, 2))
+
+    SIFT_sample.display_array(deriv[0])
+    SIFT_sample.display_array(deriv[1])
+    SIFT_sample.display_array(np.sqrt(np.square(deriv[0]) + np.square(deriv[1])))
