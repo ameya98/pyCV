3ed6d234085f7b3fd7258e1b9d4f477aadeff4f7
Ameya Daigavane <ameya.d.98@gmail.com>
2018-11-04 20:11:48 +0530

    DoG extrema finding.

--- SIFT/sift.py
+++ SIFT/sift.py
@@ -109,25 +109,66 @@ class SIFT(CV):
         return self.image_marked
 
     # obtain SIFT descriptors - the actual SIFT algorithm
-    def detect(self):
-        # Scale-space Extrema Detection - Difference of Gaussians
-        m1 = self.convolve(SIFT.gaussian_blur_matrix(2), self.image_grayscale_array)
-        m2 = self.convolve(SIFT.gaussian_blur_matrix(1), self.image_grayscale_array)
+    def detect(self, start_scale=1, octave_size=5):
+        # 1. Scale-space Extrema Detection
+        # Difference of Gaussians Pyramid
+        prev_gaussian = CV.apply_blur(self.image_grayscale_array, start_scale)
+        scale_factor = (2 ** (1/octave_size))
+        scale = start_scale * scale_factor
+        dog_octave = []
+        for gaussian_count in range(octave_size):
+            next_gaussian = CV.apply_blur(self.image_grayscale_array, scale)
+            dog_octave.append(next_gaussian - prev_gaussian)
+            prev_gaussian = next_gaussian
+            scale *= scale_factor
 
-        return np.abs(np.abs(m1 - m2) - 255)
+        # Find extrema in DoG pyramid
+        extrema_points = []
+        for dog_index, dog_matrix in enumerate(dog_octave):
+            for i in range(1, dog_matrix.shape[0] - 1):
+                for j in range(1, dog_matrix.shape[1] - 1):
+                    # 8 neighbours in the same image
+                    neighbour_pixels = []
+                    neighbour_pixels.extend([dog_matrix[i - 1][j - 1], dog_matrix[i - 1][j], dog_matrix[i - 1][j + 1],
+                                            dog_matrix[i][j - 1], dog_matrix[i][j + 1],
+                                            dog_matrix[i + 1][j - 1], dog_matrix[i + 1][j], dog_matrix[i + 1][j + 1]])
+                    # 9 neighbours below
+                    if dog_index > 0:
+                        dog_matrix_below = dog_octave[dog_index - 1]
+                        neighbour_pixels.extend([dog_matrix_below[i - 1][j - 1], dog_matrix_below[i - 1][j], dog_matrix_below[i - 1][j + 1],
+                                                dog_matrix_below[i][j - 1], dog_matrix_below[i][j], dog_matrix_below[i][j + 1],
+                                                dog_matrix_below[i + 1][j - 1], dog_matrix_below[i + 1][j], dog_matrix_below[i + 1][j + 1]])
+
+                    # 9 neighbours above
+                    if dog_index < len(dog_octave) - 1:
+                        dog_matrix_above = dog_octave[dog_index + 1]
+                        neighbour_pixels.extend([dog_matrix_above[i - 1][j - 1], dog_matrix_above[i - 1][j], dog_matrix_above[i - 1][j + 1],
+                                                dog_matrix_above[i][j - 1], dog_matrix_above[i][j], dog_matrix_above[i][j + 1],
+                                                dog_matrix_above[i + 1][j - 1], dog_matrix_above[i + 1][j], dog_matrix_above[i + 1][j + 1]])
+
+                    # check if dog_matrix[i][j] is an extrema!
+                    if dog_matrix[i][j] > max(neighbour_pixels) or dog_matrix[i][j] < min(neighbour_pixels):
+                        extrema_points.append((i, j, dog_index))
+
+            return dog_octave, extrema_points
 
 
 if __name__ == "__main__":
     np.set_printoptions(precision=1)
 
     SIFT_sample = SIFT()
-
     SIFT_sample.open_file('mike.jpg')
 
     print(SIFT_sample.image_grayscale_array)
 
-    deriv = SIFT_sample.apply_derivative(SIFT.apply_blur(SIFT_sample.image_grayscale_array, 2))
+    # deriv = SIFT_sample.apply_derivative(SIFT.apply_blur(SIFT_sample.image_grayscale_array, 2))
+    # SIFT_sample.display_array(deriv[0])
+    # SIFT_sample.display_array(deriv[1])
+    # SIFT_sample.display_array(np.sqrt(np.square(deriv[0]) + np.square(deriv[1])))
 
-    SIFT_sample.display_array(deriv[0])
-    SIFT_sample.display_array(deriv[1])
-    SIFT_sample.display_array(np.sqrt(np.square(deriv[0]) + np.square(deriv[1])))
+    dog_octave, extrema_points = SIFT_sample.detect()
+    SIFT_sample.display_array(dog_octave[0])
+    SIFT_sample.display_array(dog_octave[1])
+    SIFT_sample.display_array(dog_octave[2])
+
+    print(extrema_points)
