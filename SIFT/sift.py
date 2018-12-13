Added keypoint intensity and gradient tests. Fixed bug in DoG extrema detection.

SIFT/sift.py
@@ -18,14 +18,13 @@ class SIFT(CV):
         super().__init__()
         self.image_marked = None
 
-    # get boxed features on top of image
+    # Get boxed features on top of image
     def get_marked(self):
         return self.image_marked
 
     # obtain SIFT descriptors - the actual SIFT algorithm
-    def detect(self, start_scale=2, octave_size=5):
-        # 1. Scale-space Extrema Detection
-        # Difference of Gaussians Pyramid
+    def detect(self, start_scale=2, octave_size=5, edge_threshold=10, contrast_threshold=0.03):
+        # Construct Difference-of-Gaussians (DoG) Pyramid
         prev_gaussian = CV.apply_blur(self.image_grayscale_array, start_scale)
         scale_factor = (2 ** (1/octave_size))
         scale = start_scale * scale_factor
@@ -38,9 +37,13 @@ class SIFT(CV):
 
         # Find extrema in DoG pyramid, excluding first and last scales
         extrema_points = []
-        for dog_index, dog_matrix in enumerate(dog_octave[1:-1]):
+        for temp_index, dog_matrix in enumerate(dog_octave[1:-1]):
             for i in range(1, dog_matrix.shape[0] - 1):
                 for j in range(1, dog_matrix.shape[1] - 1):
+
+                    # Actual index in the DoG pyramid
+                    dog_index = temp_index + 1
+
                     # 8 neighbours in the same image
                     neighbour_pixels = []
                     neighbour_pixels.extend([dog_matrix[i - 1][j - 1], dog_matrix[i - 1][j], dog_matrix[i - 1][j + 1],
@@ -58,10 +61,43 @@ class SIFT(CV):
                                              dog_matrix_above[i][j - 1], dog_matrix_above[i][j], dog_matrix_above[i][j + 1],
                                              dog_matrix_above[i + 1][j - 1], dog_matrix_above[i + 1][j], dog_matrix_above[i + 1][j + 1]])
 
-                    # check if dog_matrix[i][j] is an extrema!
+                    # Check if dog_matrix[i][j] is an extrema!
                     if dog_matrix[i][j] >= max(neighbour_pixels) or dog_matrix[i][j] <= min(neighbour_pixels):
-                        extrema_points.append((i, j, dog_index + 1))
+                        extrema_points.append((i, j, dog_index))
 
+        # Find derivatives for all scales in octave
+        dog_octave_deriv = [SIFT.apply_derivative(dog_image) for dog_image in dog_octave]
+        dog_octave_deriv_x = [deriv[0] for deriv in dog_octave_deriv]
+        dog_octave_deriv_y = [deriv[1] for deriv in dog_octave_deriv]
+        temp_doublederiv = [SIFT.apply_derivative(dog_image) for dog_image in dog_octave_deriv_y]
+
+        dog_octave_doublederiv_xy = [doublederiv[0] for doublederiv in temp_doublederiv]
+        dog_octave_doublederiv_yy = [doublederiv[1] for doublederiv in temp_doublederiv]
+        dog_octave_doublederiv_xx = [SIFT.apply_derivative(dog_image)[0] for dog_image in dog_octave_deriv_x]
+
+        # Rescale all scales in octave to range 0 to 255.
+        dog_octave_rescaled = [SIFT.rescale_array(dog_image) for dog_image in dog_octave]
+
+        # Apply intensity test
+        passed_extrema_points = []
+        for extrema_point in extrema_points:
+            i, j, index = extrema_point
+
+            if dog_octave_rescaled[index][i][j] > contrast_threshold * 255:
+                passed_extrema_points.append(extrema_point)
+
+        extrema_points = passed_extrema_points
+
+        # Apply gradient test - evaluate Hessian at extrema points
+        passed_extrema_points = []
+        for extrema_point in extrema_points:
+            i, j, index = extrema_point
+
+            hessian = np.asarray([[dog_octave_doublederiv_xx[index][i][j], dog_octave_doublederiv_xy[index][i][j]],
+                                  [dog_octave_doublederiv_xy[index][i][j], dog_octave_doublederiv_yy[index][i][j]]])
+
+            if (np.trace(hessian) ** 2)/np.linalg.det(hessian) < (edge_threshold + 1)**2/edge_threshold:
+                passed_extrema_points.append(extrema_point)
+
+        extrema_points = passed_extrema_points
         return dog_octave, extrema_points
-
-

cv.py
@@ -17,7 +17,7 @@ class CV:
         self.image_grayscale_array = None
 
     @staticmethod
-    # creates a Gaussian filter matrix of shape (size, size) where size is the closest odd integer greater than 6*scale.
+    # Creates a Gaussian filter matrix of shape (size, size) where size is the closest odd integer greater than 6*scale.
     def gaussian_blur_matrix(scale=1):
         size = 2*int(3*scale) + 1
         temp1 = np.square(np.abs(np.ceil(np.mgrid[-size / 2: size / 2, -size / 2: size / 2][0])))
@@ -27,7 +27,7 @@ class CV:
         return blur_matrix
 
     @staticmethod
-    # creates a pair of Sobel operator matrices of shape (3, 3) each.
+    # Creates a pair of Sobel operator matrices of shape (3, 3) each.
     def sobel_matrix():
         operator_matrix = -np.mgrid[-1:2, -1:2]
         operator_matrix[1][1] *= 2
@@ -51,7 +51,7 @@ class CV:
         return m2_convolved
 
     @staticmethod
-    # pad array with value
+    # Pad array with value passed
     def pad_value(a, value=0, size=1):
         def pad_with(vector, pad_width, iaxis, kwargs):
             vector[:pad_width[0]] = value
@@ -61,28 +61,28 @@ class CV:
         return np.pad(a, size, pad_with)
 
     @staticmethod
-    # displays an array representing grayscale entries as a grayscale image
+    # Displays an array representing grayscale entries as a grayscale image
     def display_array(a):
         grayscale_img = Image.fromarray(a)
         grayscale_img.show()
 
     @staticmethod
-    # applies the gaussian blur of specified scale to the 2D image array
+    # Applies the gaussian blur of specified scale to the 2D image array
     def apply_blur(imgarray, scale=1):
         return CV.convolve(CV.gaussian_blur_matrix(scale), imgarray)
 
     @staticmethod
-    # applies a Sobel derivative mask to the 2D image array
+    # Applies a Sobel derivative mask to the 2D image array
     def apply_derivative(imgarray):
         return np.array([CV.convolve(CV.sobel_matrix()[0], imgarray), CV.convolve(CV.sobel_matrix()[1], imgarray)])
 
     @staticmethod
-    # rescales an array values in the range [0, 255], to obtain a grayscale representation of the image.
-    # the array minimum gets mapped to 0 (black), and the maximum gets mapped to 255 (white).
+    # Rescales an array values in the range [0, 255], to obtain a grayscale representation of the image.
+    # The array minimum gets mapped to 0 (black), and the maximum gets mapped to 255 (white).
     def rescale_array(a):
         return 255 * (a - np.min(a)) / (np.max(a) - np.min(a))
 
-    # open a image file, associate PIL objects, and get array representations
+    # Open a image file, associate PIL objects, and get array representations
     def open_file(self, image_path):
         self.image_original = Image.open(image_path)
         self.image_grayscale = self.image_original.convert('L')
@@ -91,14 +91,14 @@ class CV:
         self.image_original_array = np.asarray(self.image_original, dtype="uint32")
         self.image_grayscale_array = np.asarray(self.image_grayscale, dtype="uint32")
 
-    # get the PIL Image object tied to the original image
+    # Get the PIL Image object tied to the original image
     def get_original(self):
         return self.image_original
 
-    # show the original image
+    # Show the original image
     def show_original(self):
         self.image_original.show()
 
-    # show the grayscale image
+    # Show the grayscale image
     def show_grayscale(self):
         self.image_grayscale.show()