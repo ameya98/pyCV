274942e19d37fc116bc34fa21cf7f4a5a82d4658
Ameya Daigavane <ameya.d.98@gmail.com>
2019-01-13 18:50:37 +0530

    Added rectangle drawing correctly.

--- SIFT/sift.py
+++ SIFT/sift.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append("..")
 
 from cv import CV
-from PIL import Image
+from PIL import Image, ImageDraw
 import numpy as np
 
 
@@ -18,12 +18,25 @@ class SIFT(CV):
         super().__init__()
         self.image_marked = None
 
+    # Mark points passed as coordinate tuples on the image.
+    def mark_points(self, points, dims=(5, 5), fill="red"):
+        self.image_marked = self.image_original.copy()
+        draw = ImageDraw.Draw(self.image_marked)
+        for point in points:
+            draw.rectangle([(point[0] - dims[0]/2, point[1] - dims[1]/2),
+                            (point[0] + dims[0]/2, point[1] + dims[1]/2)], fill=fill)
+
     # Get boxed features on top of image
     def get_marked(self):
         return self.image_marked
 
+    # Show boxed features on top of image
+    def show_marked(self):
+        self.image_marked.show()
+
     # obtain SIFT descriptors - the actual SIFT algorithm
     def detect(self, start_scale=2, octave_size=5, edge_threshold=10, contrast_threshold=0.03):
+
         # Construct Difference-of-Gaussians (DoG) Pyramid
         prev_gaussian = CV.apply_blur(self.image_grayscale_array, start_scale)
         scale_factor = (2 ** (1/octave_size))
@@ -62,10 +75,10 @@ class SIFT(CV):
                                              dog_matrix_above[i + 1][j - 1], dog_matrix_above[i + 1][j], dog_matrix_above[i + 1][j + 1]])
 
                     # Check if dog_matrix[i][j] is an extrema!
-                    if dog_matrix[i][j] >= max(neighbour_pixels) or dog_matrix[i][j] <= min(neighbour_pixels):
-                        extrema_points.append((i, j, dog_index))
+                    if dog_matrix[i][j] > max(neighbour_pixels) or dog_matrix[i][j] < min(neighbour_pixels):
+                        extrema_points.append((j, i, dog_index))
 
-        # Find derivatives for all scales in octave
+        Find derivatives for all scales in octave
         dog_octave_deriv = [SIFT.apply_derivative(dog_image) for dog_image in dog_octave]
         dog_octave_deriv_x = [deriv[0] for deriv in dog_octave_deriv]
         dog_octave_deriv_y = [deriv[1] for deriv in dog_octave_deriv]
@@ -96,8 +109,12 @@ class SIFT(CV):
             hessian = np.asarray([[dog_octave_doublederiv_xx[index][i][j], dog_octave_doublederiv_xy[index][i][j]],
                                   [dog_octave_doublederiv_xy[index][i][j], dog_octave_doublederiv_yy[index][i][j]]])
 
-            if (np.trace(hessian) ** 2)/np.linalg.det(hessian) < (edge_threshold + 1)**2/edge_threshold:
+            if (np.trace(hessian) ** 2) < np.linalg.det(hessian)*((edge_threshold + 1)**2)/edge_threshold :
                 passed_extrema_points.append(extrema_point)
 
         extrema_points = passed_extrema_points
+
+        # Mark keypoints as 10px-by-10px rectangles on image
+        self.mark_points(extrema_points, dims=(5, 5))
+
         return dog_octave, extrema_points
